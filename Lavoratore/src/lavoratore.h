75c499fe6088703fb59a7fc591eed9dceef4bc3e
pierobonluca01 <pierobonluca01@gmail.com>
2021-11-26 17:27:07 +0100

    Aggiunto "Lavoratore".

---
+++ Lavoratore/src/lavoratore.h
@@ -0,0 +1,12 @@
+/*
+ * lavoratore.h
+ */
+
+#ifndef LAVORATORE_H_
+#define LAVORATORE_H_
+
+
+
+
+
+#endif /* LAVORATORE_H_ */
---
+++ Lavoratore/src/main.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : main.cpp
+// Project     : Lavoratore
+// Author      : Luca Pierobon
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
